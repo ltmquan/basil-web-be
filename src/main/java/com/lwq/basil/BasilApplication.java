8af47a85143d9ae33d5d5d30eb4bd2844495123e
Quan Luu <quanluusmacbook@Quans-MacBook-Pro.local>
2026-01-19 10:52:24 -0500

    Initial push

---
+++ src/main/java/com/lwq/basil/BasilApplication.java
@@ -0,0 +1,13 @@
+package com.lwq.basil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lwq/basil/BasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwq.basil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
